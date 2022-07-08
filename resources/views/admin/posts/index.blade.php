5070b207a99ea1663dd18b3852782f0d6708a897
ChiaraInMiniatura <chiarag295@gmail.com>
2022-07-08 18:10:19 +0200

    aggiunto delete

--- resources/views/admin/posts/index.blade.php
+++ resources/views/admin/posts/index.blade.php
@@ -4,6 +4,15 @@
 <div class="container">
 
  <h1>Index della CRUD</h1>
+
+ @if (session('post_deleted'))
+
+ <div class="alert alert-danger" role="alert">
+    {{ session('post_deleted')}}
+ </div>
+
+ @endif
+
  <table class="table">
      <thead>
        <tr>
@@ -21,8 +30,13 @@
            <th scope="row">{{ $post->id }}</th>
            <td>{{ $post->title }}</td>
            <td>
-                <a class="btn btn-warning" href="{{ route('admin.posts.show', $post)}}">VEDI</a>
+                <a class="btn btn-success" href="{{ route('admin.posts.show', $post)}}">VEDI</a>
                 <a class="btn btn-warning" href="{{route('admin.posts.edit', $post)}}">MODIFICA</a>
+                <form class="d-inline" action="{{ route('admin.posts.destroy', $post)}}" method="POST">
+                    @csrf
+                    @method('DELETE')
+                    <button type="submit" class="btn btn-danger">ELIMINA</button>
+                </form>
             </td>
 
          </tr>
